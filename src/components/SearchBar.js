264111c542b4eee12ad118f6f0df15f3ce326e9f
yadubabu <mohini548@gmail.com>
2022-06-12 20:40:37 +0530

    label updated

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -18,7 +18,7 @@ class SearchBar extends React.Component{
             <div className='search-bar ui segment'>
                 <form className='ui form' onSubmit={this.onFormSubmit}>
                     <div className='field'>
-                        <label>Video Search</label>
+                        <label>Search for a Video</label>
                         <input type="text"
                         value={this.state.term}
                         onChange={this.onInputChange}/>
